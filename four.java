e44746571e8b2e1ebbe3ff21070050626f4ec31d
OMAsati1 <omasati@gmail.com>
2024-08-04 13:46:52 +0530

    switch case in hava

---
+++ four.java
@@ -0,0 +1,18 @@
+package com.om;
+
+import java.util.Scanner;
+
+public class four
+{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String fruit = in.next();
+        switch (fruit) {
+            case "apple" -> System.out.println("doctor's nightmare");
+            case "mango" -> System.out.println("king");
+            case "banana" -> System.out.println("High protein");
+            case "orange" -> System.out.println("very juicy");
+            default -> System.out.println("pls enter valid fruit");
+        }
+    }
+}
